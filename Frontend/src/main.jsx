cecb56381eadb7f514e4530074fd8660421b64fb
JhulianDev <jhulian.dev@gmail.com>
2024-12-13 19:23:15 -0300

    Se importa archivo "styles.css" y se elimina el uso de "Normalize.css"

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import Router from './router/Router.jsx'
-import "./assets/styles/Normalize.css"
+import "./assets/styles/styles.css"
 import '@fontsource-variable/inter';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
