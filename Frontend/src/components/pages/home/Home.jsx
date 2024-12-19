7eba25ef91f2614c7105343c7a17245b1e323cef
JhulianDev <jhulian.dev@gmail.com>
2024-12-19 20:49:09 -0300

    Se importan las secciones "Hero" y "Presentacion"

--- Frontend/src/components/pages/home/Home.jsx
+++ Frontend/src/components/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import HeroV1 from '../../sections/heros/heroV1/HeroV1'
+import PresentationV1 from '../../sections/presentations/presentationV1/presentationV1'
 
 const Home = () => {
 	return (
 		<>
 			<HeroV1 />
+			<PresentationV1 />
 		</>
 	)
 }
