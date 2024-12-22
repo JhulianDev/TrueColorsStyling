aa4d67443d4dbf4f5999a6dd854a4fe136ee99d0
JhulianDev <jhulian.dev@gmail.com>
2024-12-22 18:00:43 -0300

    Se importa la sección "Sobre mí"

--- Frontend/src/components/pages/home/Home.jsx
+++ Frontend/src/components/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import AboutMeV1 from '../../sections/aboutMe/aboutMeV1/AboutMeV1'
 import HeroV1 from '../../sections/heros/heroV1/HeroV1'
 import PresentationV1 from '../../sections/presentations/presentationV1/presentationV1'
 import ServicesV1 from '../../sections/services/servicesV1/ServicesV1'
@@ -8,6 +9,7 @@ const Home = () => {
 			<HeroV1 />
 			<PresentationV1 />
 			<ServicesV1 />
+			<AboutMeV1 />
 		</>
 	)
 }
