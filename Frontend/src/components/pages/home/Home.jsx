64eb605555728029b9286fa9f891655f5af697e0
JhulianDev <jhulian.dev@gmail.com>
2024-12-22 03:03:39 -0300

    Se importa la sección de servicios

--- Frontend/src/components/pages/home/Home.jsx
+++ Frontend/src/components/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import HeroV1 from '../../sections/heros/heroV1/HeroV1'
 import PresentationV1 from '../../sections/presentations/presentationV1/presentationV1'
+import ServicesV1 from '../../sections/services/servicesV1/ServicesV1'
 
 const Home = () => {
 	return (
 		<>
 			<HeroV1 />
 			<PresentationV1 />
+			<ServicesV1 />
 		</>
 	)
 }
