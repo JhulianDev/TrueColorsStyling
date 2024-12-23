7d4e052d6763d3d0fed898a7178612696bc979c5
JhulianDev <jhulian.dev@gmail.com>
2024-12-23 05:05:42 -0300

    Se importa la sección "Newsletter"

--- Frontend/src/components/pages/home/Home.jsx
+++ Frontend/src/components/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import AboutMeV1 from '../../sections/aboutMe/aboutMeV1/AboutMeV1'
 import HeroV1 from '../../sections/heros/heroV1/HeroV1'
+import NewsletterV1 from '../../sections/newsletters/newsletterV1/NewsletterV1'
 import PresentationV1 from '../../sections/presentations/presentationV1/presentationV1'
 import ServicesV1 from '../../sections/services/servicesV1/ServicesV1'
 
@@ -10,6 +11,7 @@ const Home = () => {
 			<PresentationV1 />
 			<ServicesV1 />
 			<AboutMeV1 />
+			<NewsletterV1 />
 		</>
 	)
 }
