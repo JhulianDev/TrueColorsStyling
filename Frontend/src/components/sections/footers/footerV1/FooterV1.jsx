22151f5bfde5badf16a6dc5e506fd0a31b329f3d
JhulianDev <jhulian.dev@gmail.com>
2024-12-23 05:26:30 -0300

    Se crea la sección "FooterV1" y sus estilos

---
+++ Frontend/src/components/sections/footers/footerV1/FooterV1.jsx
@@ -0,0 +1,11 @@
+const FooterV1 = () => {
+  return (
+    <footer className="footer bg-greenIntense-100">
+      <div className='max-width-footer'>
+        <h2 className="text-xl text-white text-center">© Copyright 2024 True Colors Styling | Web diseñada por <a href="https://aismaba.com" className="font-bold" target="_blank" rel="noopener noreferrer">aismaba.com</a></h2>
+      </div>
+    </footer>
+  );
+};
+
+export default FooterV1;
