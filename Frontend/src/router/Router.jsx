c29a7c9bc2f869b21eefd853f5356301f9a1fd94
JhulianDev <jhulian.dev@gmail.com>
2024-12-24 17:15:14 -0300

    Se agrega path para la pestaña "/coaching-de-imagen"

--- Frontend/src/router/Router.jsx
+++ Frontend/src/router/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import HeaderV1 from '../components/sections/headers/headerV1/HeaderV1'
 import Home from '../components/pages/home/Home'
 import Error404 from '../components/pages/error404/Error404'
+import CoachingDeImagen from '../components/pages/services/coachingDeImagen/CoachingDeImagen'
 
 const Router = createBrowserRouter([
 	{
@@ -12,6 +13,10 @@ const Router = createBrowserRouter([
 			{
 				index: true,
 				element: <Home />,
+			},
+			{
+				path: "/coaching-de-imagen",
+				element: <CoachingDeImagen />,
 			},
 		],
 	},
