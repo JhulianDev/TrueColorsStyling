e6e9069714d1caf640e59e942d266b3aa50e7aaf
JhulianDev <jhulian.dev@gmail.com>
2024-12-26 14:57:58 -0300

    Se importa el contenido a la prop "data" de la sección "PresentationV1"

--- Frontend/src/components/pages/services/coachingDeImagen/CoachingDeImagen.jsx
+++ Frontend/src/components/pages/services/coachingDeImagen/CoachingDeImagen.jsx
@@ -1,8 +1,13 @@
+import { data } from "../../../../content/coachingData";
 import HeroV2 from "../../../sections/heros/heroV2/HeroV2";
+import PresentationV1 from "../../../sections/presentations/presentationV1/PresentationV1";
 
 const CoachingDeImagen = () => {
   return (
-    <HeroV2 />
+    <>
+      <HeroV2 />
+      <PresentationV1 data={data.presentation} />
+    </>
   );
 };
 
