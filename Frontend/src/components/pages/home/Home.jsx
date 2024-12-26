e0c1c2ea113f8cdb25470a2197816cd1e685f3b3
JhulianDev <jhulian.dev@gmail.com>
2024-12-26 14:57:18 -0300

    Se importa el contenido a la prop "data" de la sección "PresentationV1"

--- Frontend/src/components/pages/home/Home.jsx
+++ Frontend/src/components/pages/home/Home.jsx
@@ -1,15 +1,16 @@
+import { data } from '../../../content/homeData'
 import AboutMeV1 from '../../sections/aboutMe/aboutMeV1/AboutMeV1'
 import FooterV1 from '../../sections/footers/footerV1/FooterV1'
 import HeroV1 from '../../sections/heros/heroV1/HeroV1'
 import NewsletterV1 from '../../sections/newsletters/newsletterV1/NewsletterV1'
-import PresentationV1 from '../../sections/presentations/presentationV1/presentationV1'
+import PresentationV1 from '../../sections/presentations/presentationV1/PresentationV1'
 import ServicesV1 from '../../sections/services/servicesV1/ServicesV1'
 
 const Home = () => {
 	return (
 		<>
 			<HeroV1 />
-			<PresentationV1 />
+			<PresentationV1 data={data.presentation} />
 			<ServicesV1 />
 			<AboutMeV1 />
 			<NewsletterV1 />
