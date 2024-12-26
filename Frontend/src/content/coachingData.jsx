e08e4d5b58f128c2450a5112645b8ca6b4b6f447
JhulianDev <jhulian.dev@gmail.com>
2024-12-26 14:54:53 -0300

    Se crea archivo para manejar el contenido de la página Coaching de imagen

---
+++ Frontend/src/content/coachingData.jsx
@@ -0,0 +1,27 @@
+import PRESENTACION_IMG from '../assets/images/coaching_de_imagen/presentation_image.png'
+
+export const data = {
+  hero: {
+  },
+  presentation: {
+    img: PRESENTACION_IMG,
+    border: false,
+    imageFirst: false,
+    paragraph1: (
+      <>
+        En nuestro <b className='text-greenIntense-100'>Método Ilumina tu imagen en 5 Pasos</b>, te acompañamos a descubrir el estilo que mejor refleja tu personalidad y resalta tu belleza natural.
+      </>
+    ),
+    subtitle: "Por eso en true colors styling:",
+    paragraph2: (
+      <>
+        <b>ENTENDEMOS QUE CADA PERSONA ES ÚNICA, POR ESO NUESTRO ENFOQUE ES TOTALMENTE PERSONALIZADO</b> basado en el Método Ilumina Tu Imagen; 5 pasos para una Imagen Plena y Total
+      </>
+    ),
+    paragraph3: (
+      <>
+        Trabajamos contigo para crear una <b>imagen auténtica y segura</b>, que te haga sentir bien en <b>cada aspecto de tu vida.</b>
+      </>
+    ),
+  },
+}
