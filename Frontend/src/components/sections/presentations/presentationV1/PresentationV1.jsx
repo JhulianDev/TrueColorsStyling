88dc7caecce3998e6c60362b4d077d68ae107824
JhulianDev <jhulian.dev@gmail.com>
2024-12-26 14:55:56 -0300

    Se modifica el componente para hacerlo reutilizable y reciba el contenido a traves de la prop "data"

--- Frontend/src/components/sections/presentations/presentationV1/PresentationV1.jsx
+++ Frontend/src/components/sections/presentations/presentationV1/PresentationV1.jsx
@@ -1,20 +1,18 @@
-import PRESENTACION_IMG from '../../../../assets/images/home/presentacion_image.png'
-
-const PresentationV1 = () => {
+const PresentationV1 = ({ data }) => {
   return (
     <section className="section">
       <div className="max-width">
         <div className='flex gap-12 tablet:flex-col tablet:gap-6'>
-          <div className='w-[40%] tablet:w-full border border-grayIntense-100'>
-            <img src={PRESENTACION_IMG} alt='Paullete Berg - Fundadora' className='w-full' />
+          <div className={`w-[40%] h-fit tablet:w-full ${data.border ? 'border border-grayIntense-100' : ''} ${data.imageFirst ? 'order-1' : 'order-2 tablet:order-1'}`}>
+            <img src={data.img} alt={data.alt} className='w-full' />
           </div>
 
-          <div className='w-[60%] text-grayIntense-100 flex flex-col justify-center text-justify tablet:w-full'>
-            <p className='text-3xl font-medium break-words hyphens-auto laptop:text-2xl'>Sabemos que encontrar <b className='text-greenIntense-100'>un estilo que refleje tu verdadera esencia y te haga sentir segura</b> puede ser un desafío. Es común sentirse abrumada por las opciones y desanimada cuando la ropa en tu closet no te representa.</p>
-            <h2 className='text-greenIntense-100 text-2xl uppercase mt-5 font-extrabold break-words hyphens-auto laptop:text-xl'>Por eso en true colors styling:</h2>
-            <p className='text-2xl mt-3 break-words hyphens-auto laptop:text-xl'>Te ayudamos a <b>superar esas barreras</b>, guiándote y acompañándote en cada paso para descubrir los colores y estilos que <b>mejor te quedan</b>.</p>
+          <div className={`w-[60%] text-grayIntense-100 flex flex-col justify-center text-justify tablet:w-full ${data.imageFirst ? 'order-2' : 'order-1 tablet:order-2'}`}>
+            <p className='text-3xl font-medium break-words hyphens-auto laptop:text-2xl'>{data.paragraph1}</p>
+            <h2 className='text-greenIntense-100 text-2xl uppercase mt-5 font-extrabold break-words hyphens-auto laptop:text-xl'>{data.subtitle}</h2>
+            <p className='text-2xl mt-3 break-words hyphens-auto laptop:text-xl'>{data.paragraph2}</p>
             <hr class="border-t border-grayIntense-100 mt-6 mb-5" />
-            <p className='text-2xl break-words hyphens-auto laptop:text-xl'>Junt@s, trabajamos y creamos un estilo que no solo te encante, sino que también <b>fortalezca tu confianza y te haga sentir auténtic@</b> en cualquier situación.</p>
+            <p className='text-2xl break-words hyphens-auto laptop:text-xl'>{data.paragraph3}</p>
             <hr class="border-t border-grayIntense-100 mt-6" />
           </div>
         </div>
