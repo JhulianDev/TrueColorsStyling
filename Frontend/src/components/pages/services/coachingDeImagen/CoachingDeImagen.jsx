92939b5a65cc9335a7fd72f4d42dd94d8baa7c94
JhulianDev <jhulian.dev@gmail.com>
2024-12-27 19:52:05 -0300

    Se importa la sección StepsV1

--- Frontend/src/components/pages/services/coachingDeImagen/CoachingDeImagen.jsx
+++ Frontend/src/components/pages/services/coachingDeImagen/CoachingDeImagen.jsx
@@ -2,6 +2,7 @@ import { data } from "../../../../content/coachingData";
 import BenefitsV1 from "../../../sections/benefits/benefitsV1/BenefitsV1";
 import HeroV2 from "../../../sections/heros/heroV2/HeroV2";
 import PresentationV1 from "../../../sections/presentations/presentationV1/PresentationV1";
+import StepsV1 from "../../../sections/steps/stepsV1/StepsV1";
 
 const CoachingDeImagen = () => {
   return (
@@ -9,6 +10,7 @@ const CoachingDeImagen = () => {
       <HeroV2 />
       <PresentationV1 data={data.presentation} />
       <BenefitsV1 data={data.benefits} />
+      <StepsV1 data={data.steps} />
     </>
   );
 };
