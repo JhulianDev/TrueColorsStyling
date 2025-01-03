17db85f267f8eafc84d14891da72e5186a3803c8
JhulianDev <jhulian.dev@gmail.com>
2025-01-03 03:00:17 -0300

    Se agrega la sección "PacksV2" correspondiente a los servicios individuales

--- Frontend/src/components/pages/services/coachingDeImagen/CoachingDeImagen.jsx
+++ Frontend/src/components/pages/services/coachingDeImagen/CoachingDeImagen.jsx
@@ -2,6 +2,7 @@ import { data } from "../../../../content/coachingData";
 import BenefitsV1 from "../../../sections/benefits/benefitsV1/BenefitsV1";
 import HeroV2 from "../../../sections/heros/heroV2/HeroV2";
 import PacksV1 from "../../../sections/packs/packsV1/PacksV1";
+import PacksV2 from "../../../sections/packs/packsV2/PacksV2";
 import PresentationV1 from "../../../sections/presentations/presentationV1/PresentationV1";
 import StepsV1 from "../../../sections/steps/stepsV1/StepsV1";
 
@@ -13,6 +14,7 @@ const CoachingDeImagen = () => {
       <BenefitsV1 data={data.benefits} />
       <StepsV1 data={data.steps} />
       <PacksV1 data={data.packs} />
+      <PacksV2 data={data.packsIndividuals} />
     </>
   );
 };
